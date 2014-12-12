a2c1148abcdb2742e05820a1c9ee92976979e849
Thaledric <thaledric@gmail.com>
2014-12-12 19:30:28 +0100

    Mise en page (VRAIMENT presque finale)

--- js/gui.js
+++ js/gui.js
@@ -5,13 +5,43 @@
 
 $(document).ready(
 function(){
-    // footer
-    var footer_disp = false;
+    // Overlap
     
-    var footer = $('main-footer');
+    var mainWrapper = $('.main-wrapper');
+    var undergame = $('.undergame');
+    var outspread_class = 'outspread';
+    var outspread_duration = 500;
+    var outspread_timeout;
+    var animationDone_class = 'slideDown-done'
+    
+    var o = false;
     
     
+    function hideOverlap(){
+        outspread_timeout = setTimeout(
+            function(){
+                undergame.addClass(animationDone_class);
+            }
+            , outspread_duration
+        );
+        
+        
+        mainWrapper.removeClass(outspread_class);
+    }
     
+    function showOverlap(){
+        undergame.removeClass(animationDone_class);
+        mainWrapper.addClass(outspread_class);
+        clearTimeout(outspread_timeout);
+    }
+    
+    $(document).click(
+    function(){
+        o = !o;
+        if(o) showOverlap();
+        else hideOverlap();
+    }
+    );
     
 }
 );

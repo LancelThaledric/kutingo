4b9d09631b440903542c349cc3cb1184f2b7376a
Thaledric <thaledric@gmail.com>
2014-12-12 12:16:09 +0100

    Premier commit

---
+++ gui.js
@@ -0,0 +1,17 @@
+/*
+    gui.js
+    Make the GUI out-game (website) userfriendly !
+*/
+
+$(document).ready(
+function(){
+    // footer
+    var footer_disp = false;
+    
+    var footer = $('main-footer');
+    
+    
+    
+    
+}
+);
